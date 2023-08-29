019d13dd388c70e534709287bc3a487dd6ef5f97
Idioblast <47544178+Idiob1ast@users.noreply.github.com>
2023-08-28 19:16:47 -0700

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  stringToRepeat = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  while (num > 0) {
+    stringToRepeat += string;
+    num--;
+  }
+  return stringToRepeat;
 };
 
 // Do not edit below this line
