4517bc6892f12b074d554899d830a71120b1258e
Idioblast <47544178+Idiob1ast@users.noreply.github.com>
2023-08-29 11:41:45 -0700

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let stringToReverse = [];
+  for (const character of string) {
+    stringToReverse.push(character);
+  }
+  let reversedString = stringToReverse.reverse();
+  let newString = reversedString.join("");
+  return newString;
 };
 
 // Do not edit below this line
