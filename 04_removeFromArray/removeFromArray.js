5d604f23b9f0ff872bb6ec2907b6f51dc482f280
Idioblast <47544178+Idiob1ast@users.noreply.github.com>
2023-08-30 19:49:51 -0700

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...theArguments) {
+  // Create a new array to push our elements into
+  newArray = [];
 
+  // For each item in the array, push that item to the new array if it isn't an argument provided
+
+  array.forEach((item) => {
+    if (!theArguments.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+  // Return the new array
+  return newArray;
 };
 
 // Do not edit below this line
