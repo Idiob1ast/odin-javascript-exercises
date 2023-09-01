75b861ed0606517e4d6995e51368f59ffefb6b13
Idioblast <47544178+Idiob1ast@users.noreply.github.com>
2023-09-01 13:26:00 -0700

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (thing) {
+  strippedString = thing.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return strippedString === strippedString.split("").reverse().join("");
 };
 
 // Do not edit below this line
