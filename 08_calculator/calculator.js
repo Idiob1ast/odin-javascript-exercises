6142736fddfa32a649d66cb5061566ff0d3022f2
Idioblast <47544178+Idiob1ast@users.noreply.github.com>
2023-09-01 12:39:49 -0700

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let total = 0;
+  for (const number of numbers) {
+    total += number;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let total = 1;
+  for (const number of numbers) {
+    total *= number;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  // A factorial represents the multiplication of all numbers between 1 and n (n!).
+  // So if you were to have 3! , for example, you'd compute 3 x 2 x 1 (which = 6)
+
+  // FOR EACH NUMBER BETWEEN NUMBER AND 1, multiply EACH NUMBER
+  let total = 1;
+  if (number === 0 || number === 1) {
+    return total;
+  }
+  while (number > 0) {
+    total *= number;
+    number--;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +49,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
