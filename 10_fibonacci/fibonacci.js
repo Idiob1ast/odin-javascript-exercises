58d62527185a97a9913a0157e3b6606bac9af307
Idioblast <47544178+Idiob1ast@users.noreply.github.com>
2023-09-05 12:23:52 -0700

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+  if (position < 0) {
+    return "OOPS";
+  }
+  if (position === 0) {
+    return 0;
+  }
 
+  let test1 = 1;
+  let test2 = 0;
+
+  //   Start at 2nd position
+  for (let index = 2; index <= position; index++) {
+    let current = test1 + test2;
+    test2 = test1;
+    test1 = current;
+  }
+  return test1;
 };
 
 // Do not edit below this line
